22a9f4b7f34c7df44bd829db82964e36335384af
adityapai05 <adityapai.main@gmail.com>
2024-12-08 21:53:38 +0530

    deployment error fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
     themes: [
       "light",
